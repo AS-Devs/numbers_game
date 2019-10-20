446762fd749499a7d7c6b84aa4e94d37790e4430
susanta96 <susant.vanu7278@gmail.com>
2019-10-21 00:38:27 +0530

    master: screen 1 card added

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:numbers_game/webapis/services.dart';
+import 'package:numbers_game/models/fact.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(NumbersGame());
 
-class MyApp extends StatelessWidget {
+class NumbersGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Numbers Game',
+      home: FisrtScreen(title: 'Numbers Game'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class FisrtScreen extends StatefulWidget {
+  FisrtScreen({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _FisrtScreenState createState() => _FisrtScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _FisrtScreenState extends State<FisrtScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Card(
+            elevation: 5.00,
+            margin: EdgeInsets.all(23.0),
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Column( 
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    contentPadding: EdgeInsets.all(20),
+                    leading: Icon(Icons.library_books),
+                    title: Text('Title'),
+                    subtitle: Text('Subs'),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      print('Button Pressed');
+                    },
+                    textColor: Colors.white,
+                    padding: const EdgeInsets.all(0.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0)),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: <Color>[
+                              Color(0xFFBC9CFF),
+                              Color(0xFF8BA4F9),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                      padding: const EdgeInsets.fromLTRB(23, 15, 23, 15),
+                      child: const Text('TRY AGAIN',
+                          style: TextStyle(fontSize: 16, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold, letterSpacing: 0.4 )),
+                    ),
+                  )
+                ],
+              ),
+            )),
       ),
     );
   }
